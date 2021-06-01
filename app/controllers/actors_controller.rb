689160881b96109abe0609c7dca1952d1b71c516
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-06-01 14:49:08 -0600

    cleaned up slightly

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,8 +1,7 @@
 class ActorsController < ApplicationController
   def show
     @actor = Actor.find(params[:id])
-    movies = @actor.movies.pluck(:id)
-    @actors = Actor.joins(:actor_movies).where(actor_movies: {movie_id: movies}).pluck('actors.name').uniq
+    @actors = Actor.joins(:actor_movies).where(actor_movies: {movie_id: @actor.movies.pluck(:id)}).pluck('actors.name').uniq
     @actors.delete(@actor.name)
     #sql = "SELECT movies.id FROM movies INNER JOIN actor_movies ON actor_movies.movie_id = movies.id INNER JOIN actors ON actors.id = actor_movies.actor_id WHERE actors.id = 9;"
   end
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,5 @@ class Movie < ApplicationRecord
   belongs_to :studio
   has_many :actor_movies, dependent: :destroy
   has_many :actors, through: :actor_movies
+  scope :movie_actors, -> {actors}
 end
