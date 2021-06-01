9422d3904eea4396aec37234332eed01b2643f89
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-06-01 12:49:10 -0600

    fishished extension, man do I need to practice..

--- app/controllers/actors_controller.rb
+++ app/controllers/actors_controller.rb
@@ -1,7 +1,9 @@
 class ActorsController < ApplicationController
   def show
     @actor = Actor.find(params[:id])
+    movies = @actor.movies.pluck(:id)
+    @actors = Actor.joins(:actor_movies).where(actor_movies: {movie_id: movies}).pluck('actors.name').uniq
+    @actors.delete(@actor.name)
     #sql = "SELECT movies.id FROM movies INNER JOIN actor_movies ON actor_movies.movie_id = movies.id INNER JOIN actors ON actors.id = actor_movies.actor_id WHERE actors.id = 9;"
-    #binding.pry
   end
 end
