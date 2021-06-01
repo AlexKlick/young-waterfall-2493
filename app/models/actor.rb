9320c3290c0371c639dc7b8fed7b952d51ae436e
Alex Klick <alexmac@Alexs-MacBook-Air.local>
2021-06-01 14:51:52 -0600

    worked_with class method

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -1,4 +1,8 @@
 class Actor < ApplicationRecord
   has_many :actor_movies, dependent: :destroy
   has_many :movies, through: :actor_movies
+
+  def self.worked_with(actor)
+    joins(:actor_movies).where(actor_movies: {movie_id: actor.movies.pluck(:id)}).pluck('actors.name').uniq
+  end
 end
